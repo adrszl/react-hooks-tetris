4940117c50dd1127bfb0ec320c8f2899d3a7960a
Adrian Szlegel <adrian.szlegel@hotmail.com>
2021-05-08 13:52:29 +0200

    sweepRows hook added & prettier applied

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,38 +1,54 @@
-import { useState, useEffect } from 'react';
-import { createStage } from '../gameHelpers';
+import { useState, useEffect } from "react";
+import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
-    const [stage, setStage] = useState(createStage());
+  const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
-    useEffect(() => {
-        const updateStage = prevStage => {
-            // First flush the stage
-            const newStage = prevStage.map(row => 
-                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell ))
-            );
+  useEffect(() => {
+    setRowsCleared(0);
 
-            // Then draw the tetromino
-            player.tetromino.forEach((row, y) => {
-                row.forEach((value, x) => {
-                    if(value !== 0) {
-                        newStage[y + player.pos.y][x + player.pos.x] = [
-                            value,
-                            `${player.collided ? 'merged' : 'clear'}`,
-                        ];
-                    }
-                });
-            });
+    const sweepRows = (newStage) =>
+      newStage.reduce((ack, row) => {
+        // check if all the cell in the row are filled (then clear this row and push all the cells down)
+        if (row.findIndex((cell) => cell[0] === 0) === -1) {
+          setRowsCleared((prev) => prev + 1);
+          ack.unshift(new Array(newStage[0].length).fill([0, "clear"]));
+          return ack;
+        }
+        ack.push(row);
+        return ack;
+      }, []);
 
-            // Then check if we collided
-            if(player.collided) {
-                resetPlayer();
-            }
-            
-            return newStage;
-        };
+    const updateStage = (prevStage) => {
+      // First flush the stage
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
 
-        setStage(prev => updateStage(prev));
-    }, [player, resetPlayer]);
+      // Then draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? "merged" : "clear"}`,
+            ];
+          }
+        });
+      });
 
-    return [stage, setStage];
-}
+      // Then check if we collided
+      if (player.collided) {
+        resetPlayer();
+        return sweepRows(newStage);
+      }
+
+      return newStage;
+    };
+
+    setStage((prev) => updateStage(prev));
+  }, [player, resetPlayer]);
+
+  return [stage, setStage, rowsCleared];
+};
